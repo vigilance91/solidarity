69e830fee8c47a22c75a895b204ee2a0907c6df4
Tyler Drury <that_canadianguy@hotmail.com>
2021-03-04 23:58:12 -0500

    Update Bytes32Constraints.sol
    typo

--- constraints/Bytes32Constraints.sol
+++ constraints/Bytes32Constraints.sol
@@ -5,13 +5,13 @@ pragma solidity ^0.6.2;
 import "https://github.com/vigilance91/solidarity/utils/Bytes32Logic.sol";
 import "https://github.com/vigilance91/solidarity/constraints/LogicConstraints.sol";
 
-/// @title Bytes32 Contraints
+/// @title Bytes32 Constraints
 /// @author Tyler R. Drury - 3/1/2021, All Rights Reserved
 /// @notice This library provides trivial functions for constaining the state of the EVM (using require) based on the bytes32 data type.
 /// @dev This library is base on utils Btyes32Logic and is NOT vulnerable to timing attacks,
 /// and is designed and intended for use in cryptographically secure applications,
 /// which require constraining the state of the EVM for Bytes32 types, reverting on failure.
-library Bytes32Contraints
+library Bytes32Constraints
 {
     //using LogicConstraints for bool;
     using Bytes32Logic for bytes32;
