2fda5c2888e875ce6f50ca683c19ce05654b2018
Tyler Drury <that_canadianguy@hotmail.com>
2021-04-12 23:00:36 -0400

    Update iMutableTokenSupply.sol

--- contracts/token/TokenSupply/MutableTokenSupply/iMutableTokenSupply.sol
+++ contracts/token/TokenSupply/MutableTokenSupply/iMutableTokenSupply.sol
@@ -3,7 +3,7 @@
 pragma solidity >=0.6.4 <0.8.0;
 pragma experimental ABIEncoderV2;
 
-import "https://github.com/vigilance91/solidarity/contracts/token/TokenSupply/iStaticTokenSupply.sol";
+import "https://github.com/vigilance91/solidarity/contracts/token/TokenSupply/StaticTokenSupply/iStaticTokenSupply.sol";
 ///
 /// @title Mutable Token Supply Interface
 /// @author Tyler R. Drury <vigilstudios.td@gmail.com> (www.twitter.com/StudiosVigil) - copyright 3/1/2021, All Rights Reserved
