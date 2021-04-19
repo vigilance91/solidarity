16d3fe752073b726a47ea3e6384a5bcdac61f624
Tyler Drury <that_canadianguy@hotmail.com>
2021-04-19 07:29:42 -0400

    Update MutableSupplyTokenABC.sol

--- contracts/token/MutableSupplyTokenABC.sol
+++ contracts/token/MutableSupplyTokenABC.sol
@@ -10,18 +10,15 @@ import "https://github.com/vigilance91/solidarity/contracts/token/TokenSupply/Mu
 /// @author Tyler R. Drury <vigilstudios.td@gmail.com> (www.twitter.com/StudiosVigil) - copyright 3/1/2021, All Rights Reserved
 /// @dev Abstract Base Contract for a token with a dynamic supply
 ///
-abstract contract MutableSupplyTokenABC is TokenIdentifierABC,
-    MutableTokenSupplyABC
+abstract contract MutableSupplyTokenABC is MutableTokenSupplyABC
 {
     constructor(
         string memory name,
         string memory symbol,
         uint256 initialSupply
     )internal
-        TokenIdentifierABC(name,symbol)
-        MutableTokenSupplyABC(
-            initialSupply
-        )
+        TokenIdentiferABC(name, symbol)
+        MutableTokenSupplyABC(initialSupply)
     {
     }
 }
