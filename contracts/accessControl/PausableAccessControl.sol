51196ae237cc4abbb1627ee54da5a55b3e2cd1b5
Tyler Drury <that_canadianguy@hotmail.com>
2021-05-03 14:45:45 -0400

    Create PausableAccessControl.sol
    AccessControl but with added ability to for addresses assigned the pauser role able to pause and unpause a contract, useful to halt a contract when a bug or exploit is detected in order to minimize losses

---
+++ contracts/accessControl/PausableAccessControl.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity >=0.6.4 <0.8.0;
+pragma experimental ABIEncoderV2;
+
+import "https://github.com/vigilance91/solidarity/contracts/accessControl/AccessControl.sol";
+import "https://github.com/vigilance91/solidarity/contracts/Pausable.sol";
+///
+/// @title Pausable Access Control
+/// @author Tyler R. Drury <vigilstudios.td@gmail.com> (www.twitter.com/StudiosVigil) - copyright 22/4/2021, All Rights Reserved
+///
+abstract contract PausableAccessControl is AccessControl,
+    Pausable
+{
+    //string private constant _NAME = ' PausableAccessControl: ';
+    
+    bytes32 public constant PAUSER_ROLE = keccak256("ACCESS_CONTROL.PAUSER_ROLE");
+    
+    constructor(
+    )internal 
+        AccessControl()
+        Pausable()
+    {
+        _setupRole(PAUSER_ROLE, _msgSender());
+    }
+    ///
+    /// @dev Pauses Contract
+    ///
+    /// Requirements:
+    ///     - the caller must have `PAUSER_ROLE`
+    ///
+    function pause(
+    )public virtual
+        //onlyRole(PAUSER_ROLE)
+    {
+        require(
+            hasRole(PAUSER_ROLE, _msgSender())
+            //_NAME.concatenate("must have pauser role to pause")
+        );
+        _pause();
+    }
+    ///
+    /// @dev Unpauses Contract
+    ///
+    /// Requirements:
+    ///     - the caller must have `PAUSER_ROLE`
+    ///
+    function unpause(
+    )public virtual
+        //onlyRole(PAUSER_ROLE)
+    {
+        require(
+            hasRole(PAUSER_ROLE, _msgSender())
+            //_NAME.concatenate("must have pauser role to unpause")
+        );
+        _unpause();
+    }
+}
