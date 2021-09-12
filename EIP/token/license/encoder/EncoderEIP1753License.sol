36fc149500faa812f2225d4f6d8780deded03340
Tyler Drury <that_canadianguy@hotmail.com>
2021-09-12 04:06:09 -0400

    Update EncoderEIP1753License.sol

--- EIP/token/license/encoder/EncoderEIP1753License.sol
+++ EIP/token/license/encoder/EncoderEIP1753License.sol
@@ -3,21 +3,32 @@
 pragma solidity >=0.6.4 <0.8.0;
 pragma experimental ABIEncoderV2;
 
+import "https://github.com/vigilance91/solidarity/contracts/token/TokenIdentifier/encoder/EncoderTokenIdentifierABC.sol";
 import "https://github.com/vigilance91/solidarity/EIP/token/license/encoder/EncoderEIP1753ABC.sol";
 
 import "https://github.com/vigilance91/solidarity/ERC/introspection/ERC165/ERC165.sol";
+interface iEncoderEIP1753License is iEncoderTokenIdentifier,
+    iEncoderEIP1753
+{
+    
+}
 ///
-/// @title EIP-1753 License Transaction Encoder Contract
+/// @title Allowance Transaction Encoder Contract
 /// @author Tyler R. Drury <vigilstudios.td@gmail.com> (www.twitter.com/StudiosVigil) - copyright 29/8/2021, All Rights Reserved
 /// 
 contract EncoderEIP1753License is ERC165,
+    EncoderTokenIdentifierABC,
     EncoderEIP1753ABC
-{
+{   
     constructor(
     )public
         ERC165()
+        EncoderTokenIdentifierABC()
         EncoderEIP1753ABC()
     {
+        _registerInterface(type(iEncoderTokenIdentifier).interfaceId);
         _registerInterface(type(iEncoderEIP1753).interfaceId);
+        
+        _registerInterface(type(iEncoderEIP1753License).interfaceId);
     }
 }
