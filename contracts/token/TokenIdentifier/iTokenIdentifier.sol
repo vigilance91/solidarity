998c4e42f41f6e95a49b7dc24db48b5afda07eff
Tyler Drury <that_canadianguy@hotmail.com>
2021-10-28 19:10:23 -0400

    .

--- contracts/token/TokenIdentifier/iTokenIdentifier.sol
+++ contracts/token/TokenIdentifier/iTokenIdentifier.sol
@@ -3,7 +3,7 @@
 pragma solidity >=0.6.4 <0.8.0;
 pragma experimental ABIEncoderV2;
 ///
-/// @title iTokenIndentifier
+/// @title iTokenIdentifier
 /// @author Tyler R. Drury <vigilstudios.td@gmail.com> (www.twitter.com/StudiosVigil) - copyright 3/1/2021, All Rights Reserved
 /// @dev iTokenIdentifier interface
 ///
