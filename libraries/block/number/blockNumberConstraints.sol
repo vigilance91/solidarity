b4896e7e1f790541df88fc0f9dcc94652a7dc04a
Tyler Drury <that_canadianguy@hotmail.com>
2022-01-27 14:18:43 -0500

    .

--- libraries/block/number/blockNumberConstraints.sol
+++ libraries/block/number/blockNumberConstraints.sol
@@ -3,7 +3,7 @@
 pragma solidity >=0.6.4 <0.8.0;
 pragma experimental ABIEncoderV2;
 
-import "https://github.com/vigilance91/solidarity/unsigned/uint256Constraints.sol";
+import "https://github.com/vigilance91/solidarity/libraries/unsigned/uint256Constraints.sol";
 /// 
 /// @title block.number Contraints
 /// @author Tyler R. Drury <vigilstudios.td@gmail.com> (www.twitter.com/StudiosVigil) - copyright 5/3/2021, All Rights Reserved
--- libraries/block/number/blockNumberLogic.sol
+++ libraries/block/number/blockNumberLogic.sol
@@ -3,8 +3,7 @@
 pragma solidity >=0.6.4 <0.8.0;
 pragma experimental ABIEncoderV2;
 
-import "https://github.com/vigilance91/solidarity/unsigned/uint256Constraints.sol";
-//import "Safemath.sol";
+import "https://github.com/vigilance91/solidarity/libraries/unsigned/uint256Constraints.sol";
 /// 
 /// @title block.number Logic Library for use with the chronological generation of blocks on the blockchain
 /// @author Tyler R. Drury <vigilstudios.td@gmail.com> (www.twitter.com/StudiosVigil) - copyright 6/3/2021, All Rights Reserved
